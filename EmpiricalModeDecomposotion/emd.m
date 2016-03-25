060b8adfa924b7b3de1dd4981d9d95ef322c09c7
Vladimir1994 <vladimir.matveev.1994@mail.com>
2016-03-25 19:46:34 +0300

    .

--- EmpiricalModeDecomposotion/emd.m
+++ EmpiricalModeDecomposotion/emd.m
@@ -3,11 +3,11 @@ function [imfs, decomposition] = emd(signal, E, k)
     l = length(signal);
     decomposition = zeros(l, 20);
     imfs = zeros(l, 20);
+    
+    decomposition(:, 1) = signal;
 
     if k == 0
-        decomposition(:, 1) = signal;
         i = 1;
-
         while ~is_mon(decomposition(:, i))
             imf = emdStep(decomposition(:, i), E);
             decomposition(:, i + 1) = decomposition(:, i) - imf;
@@ -16,8 +16,6 @@ function [imfs, decomposition] = emd(signal, E, k)
             toc
         end
     else
-        decomposition(:, 1) = signal;
-
         for i =1:k
             imf = emdStep(decomposition(:, i), E);
             decomposition(:, i + 1) = decomposition(:, i) - imf;
